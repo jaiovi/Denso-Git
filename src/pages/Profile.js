6b974a9436e8fcb6d3d7722abd9cbbcc0353d9c5
jaiovi <a01412442@itesm.mx>
2022-03-31 21:09:50 -0500

    Avance en Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,53 +7,8 @@ function Profile(){
     const data_examenes = [
         {name: 'Memoria', puntos:1000,},
         {name: 'Reflejos', puntos:500,},
-        {name: 'Nose Webon', puntos:1200},
+        {name: 'Chido', puntos:1200},
     ];
-    const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
-
     return(
     <div className="container">
         <div className="row">
