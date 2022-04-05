d0b4cc48dd8dff49f55f89e830d18cfc4977cf7a
A01411625 <74069437+A01411625@users.noreply.github.com>
2022-04-04 23:41:13 -0500

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -26,20 +26,76 @@ function Signup()
         console.log("e-mail: ",inputEmail.current.getValue());
         console.log("Contraseña: ",inputPass.current.getValue());
     }
+
+    const [values, setValues] = useState({      // definimos un hook que nos permite usar estados
+        nombrePila: "",
+        apellidoPat: "",
+        apellidoMat: "",
+        edad: "",
+        almaMater: "",
+        sucursal: "",
+        puesto: "",
+        email: "",                              // para cada valor establecemos un estado default
+        password: ""
+    })
+
+    const [submitted, setSubmitted] = useState(false);
+    const [valid, setValid] = useState(false);
+
+    const handleNombrePilaInputChange = (event) => {
+        setValues({...values, nombrePila: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handleApellidoPatInputChange = (event) => {
+        setValues({...values, apellidoPat: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handleApellidoMatInputChange = (event) => {
+        setValues({...values, apellidoMat: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handleEdadInputChange = (event) => {
+        setValues({...values, edad: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handleAlmaMaterInputChange = (event) => {
+        setValues({...values, almaMater: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handleSucursalInputChange = (event) => {
+        setValues({...values, sucursal: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handlePuestoInputChange = (event) => {
+        setValues({...values, puesto: event.target.value})       // the ... copies the values of "values"
+    }
+
+    const handlePasswordInputChange = (event) => {
+        setValues({...values, password: event.target.value})    // the ... copies the values of "values"
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (values.email && values.password) {
+            setValid(true);
+        }
+        setSubmitted(true);
+    }
+
     return (
         <div>
         
             <h1>Crea tu cuenta</h1>
-            <Input label="Ingresa tu nombre de pila" ref={inputNombrePila} />
-            <Input label="Ingresa tu nombre de apellido paterno" ref={inputApellidoPat} />
-            <Input label="Ingresa tu nombre de apellido materno" ref={inputApellidoMat} />
-            <Input label="Ingresa tu edad" ref={inputEdad} />
-            <Input label="Ingresa tu Alma Máter" ref={inputAlmaMater} />
-            <Input label="Ingresa la sucursal a la que estás aplicando" ref={inputSucursal} />
-            <Input label="Ingresa el puesto al que estás aplicando" ref={inputPuesto} />
-            <Input label="Ingresa tu correo e-mail" ref={inputEmail} />
-            <Input label="Ingresa tu contraseña" ref={inputPass} />
-            <Input label="Ingresa de nuevo tu contraseña" ref={inputPass} />
+            <Input label="Ingresa tu nombre de pila *" ref={inputNombrePila} />
+            <Input label="Ingresa tu nombre de apellido paterno *" ref={inputApellidoPat} />
+            <Input label="Ingresa tu nombre de apellido materno *" ref={inputApellidoMat} />
+            <Input label="Ingresa tu edad *" ref={inputEdad} />
+            <Input label="Ingresa tu Alma Máter *" ref={inputAlmaMater} />
+            <Input label="Ingresa la sucursal a la que estás aplicando *" ref={inputSucursal} />
+            <Input label="Ingresa el puesto al que estás aplicando *" ref={inputPuesto} />
+            <Input label="Ingresa tu correo e-mail *" ref={inputEmail} />
+            <Input label="Ingresa tu contraseña *" ref={inputPass} />
+            <Input label="Ingresa de nuevo tu contraseña *" ref={inputPass} />
             <Button onClick={mostrar} color={"success"}>Finalizar Registro</Button>
         </div>
     )
