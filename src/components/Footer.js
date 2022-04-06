d1d8ac749197b3a32aa6a7c8e8815bad1c9bc35b
jaiovi <a01412442@itesm.mx>
2022-04-06 00:23:20 -0500

    Boton semifuncional

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import Button from "./Button"
 function Footer(){
     return(
     <div className="p-2 bg-secondary fst-italic">
-        <p class="text-center">Equipo 2 - TC2005B</p>
-        <p class="text-center">Toda propiedad intelectual es compartida a Denso. Telefono de RH 811-xxx-xxxx</p>
+        <p className="text-center">Equipo 2 - TC2005B</p>
+        <p className="text-center">Toda propiedad intelectual es compartida a Denso. Telefono de RH 811-xxx-xxxx</p>
     </div>
     )
 }
