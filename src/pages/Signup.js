79250f8a0b5cccc5c22ccd4bcc28fa83dde80c29
rubenpablobm <a01570838@itesm.mx>
2022-04-06 22:54:47 -0500

    Update Signup.js
    Cuando intento hacer el sign up de los datos, el navegador me arroja el error:
    'Internal Server Error'

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,25 +1,25 @@
 import React, {useRef, useState} from "react";
-import { Link } from "react-router-dom"
+import { API } from "../api/API"
 import Button from "../components/Button";
 import Input from "../components/Input";
 
 function Signup()
 {
     //modificar
-    const inputName = useRef();
-    const inputLastName = useRef();
-    const inputRole = useRef();
-    const inputLocation = useRef();
-    const inputEmail = useRef();
-    const inputPass = useRef();
-    const mostrar = () =>{
-        console.log("Nombre de Pila: ",inputName.current.getValue());
-        console.log("Apellidos: ",inputLastName.current.getValue());
-        console.log("Rol: ",inputRole.current.getValue());
-        console.log("Sucursal: ",inputLocation.current.getValue());
-        console.log("e-mail: ",inputEmail.current.getValue());
-        console.log("Contraseña: ",inputPass.current.getValue());
-    }
+    // const inputName = useRef();
+    // const inputLastName = useRef();
+    // const inputRole = useRef();
+    // const inputLocation = useRef();
+    // const inputEmail = useRef();
+    // const inputPass = useRef();
+    // const mostrar = () =>{
+    //     console.log("Nombre de Pila: ",inputName.current.getValue());
+    //     console.log("Apellidos: ",inputLastName.current.getValue());
+    //     console.log("Rol: ",inputRole.current.getValue());
+    //     console.log("Sucursal: ",inputLocation.current.getValue());
+    //     console.log("e-mail: ",inputEmail.current.getValue());
+    //     console.log("Contraseña: ",inputPass.current.getValue());
+    // }
 
     const [values, setValues] = useState({      // definimos un hook que nos permite usar estados
         name: "",
@@ -27,7 +27,8 @@ function Signup()
         role: "",
         location: "",
         email: "",                              // para cada valor establecemos un estado default
-        password: ""
+        password: "",
+        password2: ""
     })
 
     const [submitted, setSubmitted] = useState(false);
@@ -58,6 +59,27 @@ function Signup()
     }
 
     const handleSubmit = (event) => {
+
+        let data = {
+            name:values.name,
+            lastName:values.lastName,
+            role:values.role,
+            location:values.location,
+            email:values.email,                              // para cada valor establecemos un estado default
+            password:values.password,
+            password2:values.password2
+        }
+
+        if ( values.password == values.password2 ){
+
+            API.post("/user", data, (response)=>{             // la dirección manda a la función "login" de user en el API
+                console.log(response)
+        
+            }, (error) => {
+                console.log(error)
+            })
+        }
+
         event.preventDefault();
         if (values.name && values.lastName && values.role && values.location && values.email && values.password) {
             setValid(true);
@@ -68,50 +90,53 @@ function Signup()
     return (
         <div className="form-container">
             <h1>Crea tu cuenta</h1>
-            <form className="register-form" onSubmit={handleSubmit}>
+            <div className="register-form">
                 {submitted && valid ? <div className="success-message"> Success! Thank you for registering </div> : null}
                 <Input
                     value = {values.name}
                     onChange = {handleNameInputChange}
                     className = "form-field"
-                    label="Ingresa tu nombre de pila *" ref={inputName} />
+                    label="Ingresa tu nombre de pila *" />
                 {submitted && !values.email ? <span>Please enter an email</span> : null}
                 <Input
                     value = {values.lastName}
                     onChange = {handleLastNameInputChange}
                     className = "form-field"
-                    label="Ingresa tu(s) apellido(s) *" ref={inputLastName} />
+                    label="Ingresa tu(s) apellido(s) *" />
                 <Input
                     value = {values.role}
                     onChange = {handleRoleInputChange}
                     className = "form-field"
-                    label="Ingresa tu rol en Denso *" ref={inputRole} />
+                    label="Ingresa tu rol en Denso *" />
                 <Input
                     value = {values.location}
                     onChange = {handleLocationInputChange}
                     className = "form-field"
-                    label="Ingresa la sucursal a la que perteneces *" ref={inputLocation} />
+                    label="Ingresa la sucursal a la que perteneces *" />
                 <Input
                     value = {values.email}
                     onChange = {handleEmailInputChange}
                     className = "form-field"
-                    label="Ingresa tu correo e-mail *" ref={inputEmail} />
+                    type="email"
+                    label="Ingresa tu correo e-mail *" />
                 <Input
                     value = {values.password}
                     onChange = {handlePasswordInputChange}
                     className = "form-field"
-                    label="Ingresa tu contraseña *" ref={inputPass} />
+                    type="password"
+                    label="Ingresa tu contraseña *" />
                 <Input
-                    value = {values.password}
+                    value = {values.password2}
                     onChange = {handlePasswordInputChange}
                     className = "form-field"
-                    label="Ingresa de nuevo tu contraseña *" ref={inputPass} />
-                <Link to={"/home"}> <Button
+                    type="password"
+                    label="Ingresa de nuevo tu contraseña *" />
+                <Button
                     className="form-field"
-                    onClick={mostrar}
                     color={"success"}
-                    type="submit" >Finalizar Registro</Button> </Link>
-            </form>
+                    onClick={handleSubmit}
+                    type="submit" >Finalizar Registro</Button>
+            </div>
         </div>
     )
 }
