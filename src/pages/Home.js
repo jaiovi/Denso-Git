373a73ba9f15f4b014e99843cce043696f63b958
jaiovi <sebastianjo2011@gmail.com>
2022-04-21 13:58:10 -0500

    Profile Actualizado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,10 @@ function Home(){
     return(
     <div>
         <h1>HOME</h1>
+        <p>CARLOS pon la tabla de todos los ususarios en HOME</p>
+
+        <i>Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
+
     </div>
     )
 }
