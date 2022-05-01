1e444707ef872063bd62f1f277b447acc096f136
jaiovi <sebastianjo2011@gmail.com>
2022-05-01 11:01:17 -0500

    Verificador Header.js adminView

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import { Link } from "react-router-dom"
 import Button from "./Button"
 
+import React, { useEffect } from 'react';
+import { API } from "../api/API"
+
 
 function Header(){
+    //VALIDADOR
+    var validador;
+    useEffect(()=>{
+        API.get("/user",(response)=>{
+            console.log(response)
+            validador=response.data.managerPerm;
+            console.log(validador)
+        })
+    },[]);
+    let adminView=null;
+    if(!validador){
+        adminView=
+        <>
+        <Link to={"/profile/my-profile"}> <Button color={"primary"}> Profile </Button> </Link>
+        <Link to={"/signup"}> <Button color={"primary"} > Sign Up </Button> </Link>
+        <Link to={"/VistaAdmin"}><Button color={"secondary"} > Vista Admin.</Button> </Link>
+        </>
+    }
     return(
 
     <div className="bg-primary d-flex">
@@ -10,10 +31,8 @@ function Header(){
         <div className="p-2 flex-fill">
             <div className="d-flex justify-content-end">
             <Link to={"/home"}> <Button color={"primary"}> Ir a <b>Home</b> </Button> </Link>
-            <Link to={"/profile/my-profile"}> <Button color={"primary"}> Profile </Button> </Link>
-            <Link to={"/login"}> <Button color={"danger"} > LogOut </Button> </Link>
-            <Link to={"/signup"}> <Button color={"primary"} > Sign Up </Button> </Link>
-            <Link to={"/VistaAdmin"}><Button color={"secondary"} > Vista Admin.</Button> </Link>
+            <Link to={"/login"}> <Button color={"danger"} > Log In </Button> </Link>
+            {adminView}
             </div>
         </div>
     </div>
