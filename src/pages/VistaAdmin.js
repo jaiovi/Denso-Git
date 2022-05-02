e1431922a3631ed498890e7ebc63327d2c42e409
jaiovi <sebastianjo2011@gmail.com>
2022-05-02 08:51:05 -0500

    Vista Admin reemplazada por componentes

--- src/pages/VistaAdmin.js
+++ src/pages/VistaAdmin.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
 import * as bootstrap from "bootstrap"
 import axios from 'axios'
 import CustomListDropDown from "../components/CustomListDropDown";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import Select from "../components/Select";
+import { API } from "../api/API"
 
 /*export default function VistaAdmin() {
     return(
@@ -16,10 +20,39 @@ import CustomListDropDown from "../components/CustomListDropDown";
 //Dropdown funcionan pero nomas muestran las opciones, no muestran nada
 
 function VistaAdmin() {
+    const inputLocation = useRef();
+    const inputDepartment = useRef();
+
+    const [message, setMessage] = useState("");
+    const [data, setData] = useState(null); //info que te entrega la API
+    const [dataKPI, setDataKPI] = useState(null);
+
+    const consultarTabla = (event) => {
+        API.get("/candidato/" + inputLocation[1] + "/" + inputDepartment[1] + "/tabla",(response)=>{ // TODO: Corregir y pregunta al profe
+            setData(response);
+            console.log(response);
+            if(response.message!="Candidatos encontrados")
+                setMessage(response.message);
+        }, (error)=>{
+            setMessage(error.message)
+        })
+        API.get("/candidato/" + inputLocation[1] + "/" + inputDepartment[1] + "/kpi",(response)=>{ // TODO: Corregir y pregunta al profe 2
+            setDataKPI(response);
+            console.log(response);
+            if(response.message!="Candidatos encontrados")
+                setMessage(response.message);
+        }, (error)=>{
+            setMessage(error.message)
+        })
+    }
+
 
     return (
         <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <Select ref={inputLocation} label={"Sucursal"} items={[{label: "-", value: "-"}, {label: "Monterrey", value: "Monterrey"}, {label: "Apodaca", value: "Apodaca"}, {label: "Guadalajara", value: "Guadalajara"}, {label: "Ciudad de México", value: "Ciudad de México"}]}/>
+            <Select ref={inputDepartment} label={"Departamento"} items={[{label: "-", value: "-"},{label: "Mecatrónica", value: "Mecatrónica"}, {label: "Mecánica", value: "Mecánica"}, {label: "Electrónica", value: "Electrónica"}, {label: "Robótica", value: "Robótica"}, {label: "Industrial y Sistemas", value: "Industrial y Sistemas"}, {label: "Tecnologías de la Información", value: "Tecnologías de la Información"}, {label: "Administración", value: "Administración"}, {label: "Contaduría", value: "Contaduría"}, {label: "Recursos Humanos", value: "Recursos Humanos"}]}/>
+            <Button onClick={consultarTabla} color={"success"}>Finalizar Registro</Button>
+            {/* <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 Departamento
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -39,8 +72,10 @@ function VistaAdmin() {
                 <li><a class="dropdown-item" href="#">Apodaca</a></li>
                 <li><a class="dropdown-item" href="#">Guadalajara</a></li>
                 <li><a class="dropdown-item" href="#">Ciudad de México</a></li>
-                </ul>
-            <title>candidato</title>
+                </ul> */}
+
+        
+        <title>candidato</title>
             <table className="table table-striped">
             <thead>
                 <tr>
