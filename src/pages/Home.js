40f9cd1ca808f65f536b014e408403e21fa0c191
jaiovi <sebastianjo2011@gmail.com>
2022-05-03 12:45:39 -0500

    Tabla adminview funcional

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,11 @@ function Home(){
     return(
     <div>
         <h1>HOME</h1>
-        <p>CARLOS pon la tabla de todos los ususarios en HOME</p>
-
-        <i>Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
-
+        <h3 className="d-flex justify-content-center">Embeded del UNITY</h3>
+        <div className="d-flex justify-content-center m-4">
+            <i>Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
+        </div>
+        
     </div>
     )
 }
