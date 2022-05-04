ec0ba3904ce995ecb19176b80f4013bdfb30ad03
jaiovi <sebastianjo2011@gmail.com>
2022-05-04 10:01:20 -0500

    Ultima modificacion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,13 @@ function Home(){
     return(
     <div>
         <h1>HOME</h1>
-        <h3 className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
+        
         <iframe frameborder="0" src="https://itch.io/embed-upload/5726298?color=333333" allowfullscreen="" width="640" height="380"><a href="https://jaiovi.itch.io/denso-minijuegos">Play Denso Minijuegos on itch.io</a></iframe>
-        </h3>
+        </div>
         <div className="d-flex justify-content-center m-4">
-            <i>Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
+        
+            <i>Embeded de Unity vacio - juego aquí. Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
         </div>
         
     </div>
