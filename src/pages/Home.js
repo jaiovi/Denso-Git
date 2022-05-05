cd5fd40771f25d612acde3459e42287563c28c53
jaiovi <sebastianjo2011@gmail.com>
2022-05-05 11:10:23 -0500

    RF4 Objetivos empresa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@
 
 function Home(){
     return(
-    <div>
-        <h1>HOME</h1>
+    <div className="container">
+        <h1 className="p-2">HOME</h1>
         <div className="d-flex justify-content-center">
         
         <iframe frameborder="0" src="https://itch.io/embed-upload/5726298?color=333333" allowfullscreen="" width="640" height="380"><a href="https://jaiovi.itch.io/denso-minijuegos">Play Denso Minijuegos on itch.io</a></iframe>
@@ -13,7 +13,14 @@ function Home(){
         
             <i>Embeded de Unity vacio - juego aquí. Se recomiendo el usa de navegadores basados en Chrome para reproducir el videojuego. Use el token id compartido por e-mail.</i>
         </div>
-        
+        <h3>MISION, VISION Y VALORES</h3>
+        <p>
+        Nuestra misión es ayudar a construir una sana y productiva sociedad a través de la participación que refleje las necesidades e intereses de nuestra compañía, nuestros empleados  y los de nuestra comunidad, además buscamos contribuir al cuidado del medio ambiente con  productos que reducen el uso de contaminantes y con procesos de reutilización, como el tratamiento del agua residual que es utilizado para el riego de las áreas verdes, también se cuenta con un “lombricario” donde se genera abono a través del tratamiento los restos de  comida provenientes de comedor, papel sanitario, podas de jardín y lodos biológicos de la planta de tratamiento de aguas residuales de la empresa.
+        </p><p>
+        Nuestro compromiso con la sociedad ha hecho que en nuestra actividad empresarial incorporemos proyectos sociales con entidades externas, actividades cómo: Actividades de voluntariado de Concientización Vial, Reforestación de parques, Donación a escuelas, Donación de sillas de ruedas entre otros.
+        </p><p>
+        Además de todo esto DENSO México es una empresa que se preocupa por la inclusión ya que un gran porcentaje de nuestro capital humano está conformado por mujeres con la convicción de desarrollarse dentro de la industria automotriz en diferentes áreas como Producción, Administración, Recursos Humanos, Ingeniería, entre otras.
+        </p>
     </div>
     )
 }
